d1be950a50710885f06c0efd5817209507e399b2
mohamed-helal1 <m.h.helal1997@gmail.com>
2022-09-10 15:45:10 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require('morgan')
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT;
+const app = express();
+
+app.use(morgan('combined'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use((_req, res) => {
+	res.status(404).json({
+		err: null,
+		msg: "The function does not exist",
+		data: null,
+	});
+});
+
+app.listen(port, () => {
+	console.log(`start app on port ${port}`);
+});
